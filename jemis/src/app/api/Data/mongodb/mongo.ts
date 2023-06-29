0e104f0dbeaaa7cff17894634d800efb8bc0d7b9
Luís Santos <130599002+Gordoxmon@users.noreply.github.com>
2023-06-29 14:44:05 +0100

    atualização do código de conexões

--- jemis/src/app/api/Data/mongodb/mongo.ts
+++ jemis/src/app/api/Data/mongodb/mongo.ts
@@ -8,62 +8,53 @@ let db: typeof Db;
 
 /* setup do client da base de dados */
 
-client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    useUnifiedTopology: true,
-    deprecationErrors: true,
-  },
-});
+async function Conn(collectionName: string) {
+  try {
+    /* declração de variavel de conexão */
+    client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        useUnifiedTopology: true,
+        deprecationErrors: true,
+      },
+    });
 
-export const ReadUser = async () => {
-  try {
+    /* conexão a base de dados */
     await client.connect();
     db = client.db(dbName);
+
+    /* verificação se a coleção existe */
     const collection = await db.listCollections().toArray();
     if (
-      collection.filter((c: { name: string }) => c.name === "Users").length ===
-      1
+      collection.filter((c: { name: string }) => c.name === collectionName)
+        .length === 1
     ) {
-      let data = db.collection("Users");
-      let result = await data.find().toArray();
-      return result;
-    } else throw new Error("Collection não encontrado");
-  } catch (err: any) {
-    console.log(err.name + ": " + err.message);
-  }
-};
-export const InsertUser = async () => {
-  try {
-    await client.connect();
-    db = client.db(dbName);
-    const collection = await db.listCollections().toArray();
-    if (
-      collection.filter((c: { name: string }) => c.name === "Users").length ===
-      1
-    ) {
-      let data = db.collection("Users");
+      let data = db.collection(collectionName);
       return data;
     } else throw new Error("Collection não encontrado");
   } catch (err: any) {
     console.log(err.name + ": " + err.message);
   }
+}
+
+export const ReadDeleteUser = async () => {
+  /* coneção a coleção */
+  const data = await Conn("Users");
+  let result = await data.find().toArray();
+  return result;
 };
-export const ReadIndagacoes = async () => {
-  try {
-    await client.connect();
-    db = client.db(dbName);
-    const collection = await db.listCollections().toArray();
-    if (
-      collection.filter((c: { name: string }) => c.name === "Users").length ===
-      1
-    ) {
-      let data = db.collection("Users");
-      let result = await data.find().toArray();
-      return result;
-    } else throw new Error("Collection não encontrado");
-  } catch (err: any) {
-    console.log(err.name + ": " + err.message);
-  }
+export const InsertUser = async () => {
+  /* retorna apenas a conexão, duvida se permanece, ou se damos importe apenas a conexão */
+  return await Conn("Users");
+};
+export const ReadDeleteIndagacoes = async () => {
+  /* coneção a coleção */
+  const data = await Conn("Indagacoes");
+  return await data.find().toArray();
+};
+
+export const InsertIndagacoes = async () => {
+  /* retorna apenas a conexão, duvida se permanece, ou se damos importe apenas a conexão */
+  return await Conn("Indagacoes");
 };
--- jemis/src/app/api/indagacoes/ler/route.ts
+++ jemis/src/app/api/indagacoes/ler/route.ts
@@ -1,9 +1,6 @@
 import { NextResponse } from "next/server";
-
 import { cookies } from "next/headers";
-
 import { redirect } from "next/navigation";
-/* começo do desenvolvimento das funçoes da base de dados */
 
 export async function GET(req: Request, res: NextResponse) {
   redirect("/");
