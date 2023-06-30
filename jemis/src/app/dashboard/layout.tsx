5d44e617d36a004ddf2e04172c81ca2d1affd898
zé <neffex21@gmail.com>
2023-06-30 15:19:19 +0100

    fim

--- jemis/src/app/dashboard/layout.tsx
+++ jemis/src/app/dashboard/layout.tsx
@@ -15,12 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <Header className=" sticky top-0 m-4 w-full h-fit flex gap-8 justify-center rounded-lg bg-zinc-800">
+      <Header className=" sticky top-0 m-4 w-full h-fit flex gap-8 justify-center rounded-lg bg-zinc-100 dark:bg-zinc-800 text-white">
         <Nav />
       </Header>
 
       {children}
-      <Footer className=" fixed bottom-0 w-full h-fit flex gap-8 justify-center bg-zinc-800" />
+      <Footer className=" fixed bottom-0 w-full h-fit flex gap-8 justify-center bg-zinc-100 dark:bg-zinc-800 " />
     </>
   );
 }
